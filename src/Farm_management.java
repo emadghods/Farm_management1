f46e87404c70eedf7971582591c2a2fb83284839
emadghods <emad.ghods@gmail.com>
2020-10-25 11:47:53 +0330

    Initial commit

---
+++ src/Farm_management.java
@@ -0,0 +1,49 @@
+
+import java.util.*;
+public class Farm_management {
+
+   public static void main ( String [] args){
+
+       Scanner vorudi = new Scanner (System.in);
+       System.out.println();
+       
+   }
+   class cow_details{
+
+           int weight;
+           int age;
+           String color;
+           cow_details ( int a, int b, String c ) {
+               this.weight=a;
+               this.age=b;
+               this.color=c;
+           }
+   }
+class sheap_details{
+               int weight;
+               int age;
+               int weightwool;
+               sheap_details(int a ,int b, int d){
+                   this. weight = a;
+                   this.age= b;
+                   this. weightwool= d;
+
+               }
+
+       }
+class dunky_details{
+       int weight;
+       int age;
+       String color;
+       int height;
+       dunky_details(int a, int b, String c,int d){
+           this.weight= a;
+           this. age=b;
+           this.color = c;
+           this.height=d;
+       }
+
+
+}
+
+}
